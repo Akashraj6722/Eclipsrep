39d98b3084618a80985762b3bcb0d36c3aed45d3
Akashraj6722 <114343719+Akashraj6722@users.noreply.github.com>
2024-04-23 11:15:29 +0530

    Update CallHosp.java

--- Day6/src/com/chainsys/day6/CallHosp.java
+++ Day6/src/com/chainsys/day6/CallHosp.java
@@ -5,61 +5,76 @@ import java.util.*;
 public class CallHosp {
 
 	public static void main(String[] args) {
-       Scanner input=new Scanner(System.in);
-		
-		Hospital hp=new Hospital();
-		
-		int hospId1;
+		Scanner input = new Scanner(System.in);
+
+		Hospital hp = new Hospital();
+
+		String hospId1;
 		String hospName1;
 		String hospLocation1;
-		
-		
+		String patientName;
+		int patientID;
+
+		String regex = "[a-zA-Z]{1,10}";
+		String regex1 = "[0-9]{1,4}";
+
 		System.out.println("Ener the Hospital ID:");
-		hospId1=input.nextInt();
-		while(true) {
-			
-			if(hospId1<0) {
-				System.out.println("Enter the valid number");
-				hospId1=input.nextInt();
-			}else {
-				
+		hospId1 = input.next();
+		while (!hospId1.matches(regex1)) {
+			System.out.println("Enter the VALID  ID:");
+			hospId1 = input.next();
 
-			}
+		}
 
-		
-			
-	    System.out.println("Enter the Hospital Name:");
-		hospName1=input.next();
-		
-		
-		
+		System.out.println("Enter the Hospital Name:");
+		hospName1 = input.next();
+		while (!hospName1.matches(regex)) {
+			System.out.println("Enter the VALID  NAME:");
+			hospName1 = input.next();
+
+		}
+
 		System.out.println("Ener the Hospital Location:");
-		hospLocation1=input.next();
-		
-		
-		
-		
-		
-		
+		hospLocation1 = input.next();
+		while (!hospLocation1.matches(regex)) {
+			System.out.println("Enter the VALID  NAME:");
+			hospLocation1 = input.next();
+
+		}
+
+		System.out.println("Ener the Patient Name:");
+		patientName = input.next();
+		while (!patientName.matches(regex)) {
+			System.out.println("Enter the VALID  NAME:");
+			patientName = input.next();
+
+		}
+
+		System.out.println("Ener the Patient's ID:");
+		patientID = input.nextInt();
+		while(patientID<0) {
+			System.out.println("Enter the VALID ID");
+			patientID = input.nextInt();
+		}
+
 		hp.setHospId(hospId1);
 		hp.setHospName(hospName1);
 		hp.setHospLocation(hospLocation1);
-		
-		
+		hp.setPatientName(patientName);
+		hp.setPatientID(patientID);
+
+		HospData hospdata = new HospData();
+		hospdata.details(hospName1, hospLocation1);
+		hospdata.details(patientName, patientID);
+
 //		
 //		System.out.println("Hospital ID: " + hp.getHospId());
 //		System.out.println("Hospital Name: "+hp.getHospName());
 //		System.out.println("Hospital Location: " + hp.getHospLocation());
 		System.out.println(hp.toString());
 		
-		
-		
-	
+		input.close();
+
 	}
-	}
+
 }
-	
-
-	
-
-
